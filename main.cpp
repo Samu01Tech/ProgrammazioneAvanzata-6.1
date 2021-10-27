e86865f6299c07a53c42cc3ff156e429004e730a
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-10-27 13:46:27 +0000

    Costr. zero parametri

--- main.cpp
+++ main.cpp
@@ -5,16 +5,29 @@ class Studente{
     private:
         int datoStudente;
     public:
+        Studente(){
+            datoStudente = 0; 
+            cout << "Creato Studente" << endl;  
+        };
 };
 class Lavoratore{
     private:
         int datoLavoratore;
     public:
+        Lavoratore(){
+            datoLavoratore = 0;
+            cout << "Creato Lavoratore" << endl;
+        };
 };
 class StudenteLavoratore: public Studente, public Lavoratore{
-
+    public:
+        StudenteLavoratore(){
+            cout << "Creato Studente Lavoratore" << endl;
+        };
 };
 
+
 int main() {
-  cout << "Hello World!\n";
+    StudenteLavoratore s1;
+    return 0;
 } 
